585b8ecd10acdafa8ee1877c8853a971f618b78c
Allen <allensratheburn@gmail.com>
2015-05-26 10:33:32 -0400

    v2

--- red.cpp
+++ red.cpp
@@ -5,18 +5,30 @@
 #include <string>
 using namespace std;
 
-int main(int argc, char** argv)
+string intro()
 {
-
 	string sent;
-	string s ="";
-	string c = "";
-	int i = 0;
 
 	cout << "Gimme a sentence: ";
 	
 	getline(cin,sent);
 	cout << "Your current sentence is: " << sent << '\n';
+
+	return sent;	
+}
+
+void ender(string s)
+{
+	cout << "Your new sentence is:\n";
+	cout << s << '\n';
+}
+
+string subscriptor(string sent)
+{
+
+	string s ="";
+	string c = "";
+	int i = 0;
 
 	while (i < sent.length())
 	{
@@ -26,11 +38,64 @@ int main(int argc, char** argv)
 		s += "^";
 	}
 
-	cout << "Your new string is:\n";
-	//s.pop_back();
-	cout << s << '\n';
+	s.pop_back();
+
+	return s;
+}
+
+void general(function<void()> intro, function<string(string sent)> lolcats)
+{
+	
+}
+
+
+
+int main(int argc, char** argv)
+{
+
+
+	char choice;
+	
+	do
+	{
+		cout << "\n============ MENU ===============\n";
+		cout << "1. Superscriptor"
+		     << "2. Bolder"
+		     << "3. Strikethrougher"
+		     << "4. Coder"
+		     << "5. Quoter"
+		     << "6. Exit\n";
+
+		cout << "\n==================================\n";
+
+	}
+	while(choice != '6');	
+
+	switch(choice)
+	{
+		case '1': //intro(); subscriptor();
+		break;
+		case '2': 
+		break;
+		case '3': 
+		break;
+		case '4': 
+		break;
+		case '5': 
+		break;
+		case '6': 
+		break;
+		default:
+		break;
+	}
+
+	ender(s);
+
 	return 0;
 }
+
+
+
 
 
 #endif
